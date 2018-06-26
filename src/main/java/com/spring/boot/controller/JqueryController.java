3fdaa5ba035cec78b6d26f0f5a7d886b705df976
dla1434 <dla1434@naver.com>
2018-06-26 11:45:06 +0900

    First Commit Jquery

---
+++ src/main/java/com/spring/boot/controller/JqueryController.java
@@ -0,0 +1,63 @@
+package com.spring.boot.controller;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.spring.boot.model.JsInfo;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class JqueryController {
+	@GetMapping("/jquery/list")
+	public String juqeryList(HttpServletRequest request, ModelMap modelMap) {
+		log.info("juqeryList");
+		
+		ArrayList arrayList = new ArrayList();
+		
+		try {
+			Path currentRelativePath = Paths.get("");
+			String s = currentRelativePath.toAbsolutePath().toString();
+			System.out.println("Current relative path is: " + s);
+			
+			Path rootDir = Paths.get("src/main/resources/static/jquery");
+			log.info("rootDir : {}, rootDir.toFile() : {}", rootDir, rootDir.toFile());
+			
+			Files.walk(rootDir)
+				.filter(Files::isRegularFile)
+				.filter(file -> {
+					String fileName = file.getFileName().toString();
+//					log.info("fileName : {}", fileName);
+					return fileName.endsWith(".html");
+				})
+				.forEach(item -> {
+					String orgPath = item.toString();
+					String replacePath = orgPath.replace("src\\main\\resources\\static\\jquery\\", "");
+					log.info("replacePath : {}", replacePath);
+					
+					JsInfo jsInfo = new JsInfo();
+					jsInfo.setName(item.getFileName().toString());
+					jsInfo.setPath(replacePath);
+					arrayList.add(jsInfo);
+				});
+		} catch (Exception e) {
+			log.error("Exception : {}", e);
+		}
+		
+		log.info("juqeryList : {}", arrayList);
+		
+		modelMap.addAttribute("page", "fragments/jquery/jqueryList");
+		modelMap.addAttribute("list", arrayList);
+		
+		return "index";
+	}
+}
