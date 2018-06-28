55f5fb99d68a8666f5b6285aba603e1d0f8b470c
dla1434 <dla1434@naver.com>
2018-06-28 17:26:52 +0900

    dashboard에서 list 페이지 메뉴 추가 jquery-ui list 추가

--- src/main/java/com/spring/boot/controller/JqueryController.java
+++ src/main/java/com/spring/boot/controller/JqueryController.java
@@ -60,4 +60,47 @@ public class JqueryController {
 		
 		return "index";
 	}
+	
+	@GetMapping("/jqueryUI/list")
+	public String juqeryUiList(HttpServletRequest request, ModelMap modelMap) {
+		log.info("juqeryUiList");
+		
+		ArrayList arrayList = new ArrayList();
+		
+		try {
+			Path currentRelativePath = Paths.get("");
+			String s = currentRelativePath.toAbsolutePath().toString();
+			System.out.println("Current relative path is: " + s);
+			
+			Path rootDir = Paths.get("src/main/resources/static/jqueryUI");
+			log.info("rootDir : {}, rootDir.toFile() : {}", rootDir, rootDir.toFile());
+			
+			Files.walk(rootDir)
+				.filter(Files::isRegularFile)
+				.filter(file -> {
+					String fileName = file.getFileName().toString();
+//					log.info("fileName : {}", fileName);
+					return fileName.endsWith(".html");
+				})
+				.forEach(item -> {
+					String orgPath = item.toString();
+					String replacePath = orgPath.replace("src\\main\\resources\\static\\jqueryUI\\", "");
+					log.info("replacePath : {}", replacePath);
+					
+					JsInfo jsInfo = new JsInfo();
+					jsInfo.setName(item.getFileName().toString());
+					jsInfo.setPath(replacePath);
+					arrayList.add(jsInfo);
+				});
+		} catch (Exception e) {
+			log.error("Exception : {}", e);
+		}
+		
+		log.info("juqeryUiList : {}", arrayList);
+		
+		modelMap.addAttribute("page", "fragments/jquery/jqueryUiList");
+		modelMap.addAttribute("list", arrayList);
+		
+		return "index";
+	}
 }
