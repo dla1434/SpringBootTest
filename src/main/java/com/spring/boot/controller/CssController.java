2e7800e408e4f64ede8d126dbcdd1bf1bcb70106
dla1434 <dla1434@naver.com>
2018-07-04 18:13:45 +0900

    20180702 : html 추가, jquery 추가, css 추가

---
+++ src/main/java/com/spring/boot/controller/CssController.java
@@ -0,0 +1,63 @@
+package com.spring.boot.controller;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.spring.boot.model.JsInfo;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class CssController {
+	@GetMapping("/css/list")
+	public String cssList(HttpServletRequest request, ModelMap modelMap) {
+		log.info("cssList");
+		
+		ArrayList arrayList = new ArrayList();
+		
+		try {
+			Path currentRelativePath = Paths.get("");
+			String s = currentRelativePath.toAbsolutePath().toString();
+			System.out.println("Current relative path is: " + s);
+			
+			Path rootDir = Paths.get("src/main/resources/static/css");
+			log.info("rootDir : {}, rootDir.toFile() : {}", rootDir, rootDir.toFile());
+			
+			Files.walk(rootDir)
+				.filter(Files::isRegularFile)
+				.filter(file -> {
+					String fileName = file.getFileName().toString();
+//					log.info("fileName : {}", fileName);
+					return fileName.endsWith(".html");
+				})
+				.forEach(item -> {
+					String orgPath = item.toString();
+					String replacePath = orgPath.replace("src\\main\\resources\\static\\css\\", "");
+					log.info("replacePath : {}", replacePath);
+					
+					JsInfo jsInfo = new JsInfo();
+					jsInfo.setName(item.getFileName().toString());
+					jsInfo.setPath(replacePath);
+					arrayList.add(jsInfo);
+				});
+		} catch (Exception e) {
+			log.error("Exception : {}", e);
+		}
+		
+		log.info("cssList : {}", arrayList);
+		
+		modelMap.addAttribute("page", "fragments/css/cssList");
+		modelMap.addAttribute("list", arrayList);
+		
+		return "index";
+	}
+}
